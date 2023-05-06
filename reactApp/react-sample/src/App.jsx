8755e32376af122266d70b2811ce0da0f410cc04
westtail <nishio.ryouta.1997@gmail.com>
2023-05-06 20:12:14 +0900

    ボタンの追加

--- reactApp/react-sample/src/App.jsx
+++ reactApp/react-sample/src/App.jsx
@@ -1,8 +1,12 @@
 export const App = () => {
+    const onClickButton = () => {
+        alert();
+    };
     return (
-        <>
+        <div>
             <h1>こんにちは</h1>
             <p>こんにちは</p>
-        </>
+            <button onClick={onClickButton}>ボタン</button>
+        </div>
     );
 };
