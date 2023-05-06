8caff5c4e5c5ce4b1ffc59c91374a9dde693e1e5
westtail <nishio.ryouta.1997@gmail.com>
2023-05-06 21:06:48 +0900

    styleの追加

--- reactApp/react-sample/src/App.jsx
+++ reactApp/react-sample/src/App.jsx
@@ -2,10 +2,20 @@ export const App = () => {
     const onClickButton = () => {
         alert();
     };
+
+    const contentStyle = {
+        color: "blue",
+        fontsize: "20px"
+    }
+    const contentPinkStyle = {
+        color: "pink",
+        fontsize: "20px"
+    }
     return (
         <div>
             <h1>こんにちは</h1>
-            <p>こんにちは</p>
+            <p style={contentStyle}>こんにちは</p>
+            <p style={contentPinkStyle}>こんにちは</p>
             <button onClick={onClickButton}>ボタン</button>
         </div>
     );
